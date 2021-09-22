2e89e551d3edff98c8edf9b33fc36137c737a38a
Liudmila Razhkavets <lrazhkavets@gmail.com>
2021-09-22 18:09:53 +0300

    test complete task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,9 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats( matrix ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let arr = matrix.flat(Infinity).filter(el => el == '^^')
+  return arr.length;
 }
